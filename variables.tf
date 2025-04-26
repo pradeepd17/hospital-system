b54ba1dd5a9e73a5c63426687168c0856d4cb6de
pradeepd17 <pradeepd17in@gmail.com>
2025-04-26 17:54:43 +0530

    Update and rename README.md to variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
